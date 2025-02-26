955623e49a164090e6f80bbc7a6d84140aa16e8f
wook <tjddnr209@naver.com>
2025-02-26 20:14:49 +0900

    스프링 웹 개발 기초

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,37 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello.html";
+    }
+
+    @GetMapping("/hello-mvc")
+    public String helloMvc(String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template.html";
+    }
+
+    @GetMapping("/hello-string")
+    @ResponseBody   // 문자열을 그대로 반환
+    public String helloString(String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
